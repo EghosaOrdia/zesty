64971ffe124821e6361f44940f76d05fe1b0e333
Eghosa Ordia <eghodia1004@yahoo.com>
2025-08-18 20:54:17 +0200

    feat: add initial RoomsPage component with a greeting message

---
+++ src/app/rooms/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RoomsPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Hello world!</h1>
+    </div>
+  );
+};
+
+export default RoomsPage;
